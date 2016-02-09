213ad6c13b3c05af7e1c9c79b82ce2c084ebbe5a
lawer <carlesgm@gmail.com>
2016-02-10 00:20:42 +0100

    Overlays

--- app/src/main/java/org/ecaib/parkingsbcn/MapFragment.java
+++ app/src/main/java/org/ecaib/parkingsbcn/MapFragment.java
@@ -3,13 +3,21 @@ package org.ecaib.parkingsbcn;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.ecaib.parkingsbcn.R;
+import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +26,11 @@ public class MapFragment extends Fragment {
 
 
     private MapView map;
+    private MyLocationNewOverlay myLocationOverlay;
+    private MinimapOverlay mMinimapOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private CompassOverlay mCompassOverlay;
+    private IMapController mapController;
 
     public MapFragment() {
         // Required empty public constructor
@@ -31,13 +44,67 @@ public class MapFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
         map = (MapView) view.findViewById(R.id.map);
+
+        initializeMap();
+        setZoom();
+        setOverlays();
+
+        map.invalidate();
+
+        return view;
+    }
+
+    private void initializeMap() {
         map.setTileSource(TileSourceFactory.MAPQUESTOSM);
         map.setTilesScaledToDpi(true);
 
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
+    }
 
-        return view;
+    private void setZoom() {
+        //  Setteamos el zoom al mismo nivel y ajustamos la posición a un geopunto
+        mapController = map.getController();
+        mapController.setZoom(15);
+    }
+
+    private void setOverlays() {
+        final DisplayMetrics dm = getResources().getDisplayMetrics();
+
+        myLocationOverlay = new MyLocationNewOverlay(
+                getContext(),
+                new GpsMyLocationProvider(getContext()),
+                map
+        );
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.runOnFirstFix(new Runnable() {
+            public void run() {
+                mapController.animateTo( myLocationOverlay
+                        .getMyLocation());
+            }
+        });
+
+/*
+        mMinimapOverlay = new MinimapOverlay(getContext(), map.getTileRequestCompleteHandler());
+        mMinimapOverlay.setWidth(dm.widthPixels / 5);
+        mMinimapOverlay.setHeight(dm.heightPixels / 5);
+*/
+
+        mScaleBarOverlay = new ScaleBarOverlay(map);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mCompassOverlay = new CompassOverlay(
+                getContext(),
+                new InternalCompassOrientationProvider(getContext()),
+                map
+        );
+        mCompassOverlay.enableCompass();
+
+        map.getOverlays().add(myLocationOverlay);
+        map.getOverlays().add(this.mMinimapOverlay);
+        map.getOverlays().add(this.mScaleBarOverlay);
+        map.getOverlays().add(this.mCompassOverlay);
     }
 
 }
