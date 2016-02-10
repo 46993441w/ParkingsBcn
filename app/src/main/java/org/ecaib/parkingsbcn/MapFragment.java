19138c25852923697b1585f52b9ddbca6be09651
lawer <carlesgm@gmail.com>
2016-02-10 01:03:46 +0100

    Markers

--- app/src/main/java/org/ecaib/parkingsbcn/MapFragment.java
+++ app/src/main/java/org/ecaib/parkingsbcn/MapFragment.java
@@ -1,16 +1,27 @@
 package org.ecaib.parkingsbcn;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.ecaib.parkingsbcn.R;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer;
+import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MinimapOverlay;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
@@ -31,6 +42,7 @@ public class MapFragment extends Fragment {
     private ScaleBarOverlay mScaleBarOverlay;
     private CompassOverlay mCompassOverlay;
     private IMapController mapController;
+    private RadiusMarkerClusterer parkingMarkers;
 
     public MapFragment() {
         // Required empty public constructor
@@ -49,9 +61,64 @@ public class MapFragment extends Fragment {
         setZoom();
         setOverlays();
 
+        putMarkers();
+
         map.invalidate();
 
         return view;
+    }
+
+    private void putMarkers() {
+        setupMarkerOverlay();
+
+        MyApp app = (MyApp) getActivity().getApplication();
+        Firebase ref = app.getRef();
+
+        final Firebase parkings = ref.child("parkings");
+
+        parkings.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    Park parking = postSnapshot.getValue(Park.class);
+                    Log.e("XXXX", parking.toString());
+
+                    Marker marker = new Marker(map);
+
+                    GeoPoint point = new GeoPoint(
+                            Double.parseDouble(parking.getLat()),
+                            Double.parseDouble(parking.getLon())
+                    );
+
+                    marker.setPosition(point);
+
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    marker.setIcon(getResources().getDrawable(R.drawable.parking));
+                    marker.setTitle(parking.getName());
+                    marker.setAlpha(0.6f);
+
+                    parkingMarkers.add(marker);
+                }
+                parkingMarkers.invalidate();
+                map.invalidate();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+    }
+
+    private void setupMarkerOverlay() {
+        parkingMarkers = new RadiusMarkerClusterer(getContext());
+        map.getOverlays().add(parkingMarkers);
+
+        Drawable clusterIconD = getResources().getDrawable(R.drawable.marker_cluster);
+        Bitmap clusterIcon = ((BitmapDrawable)clusterIconD).getBitmap();
+
+        parkingMarkers.setIcon(clusterIcon);
+        parkingMarkers.setRadius(100);
     }
 
     private void initializeMap() {
@@ -63,9 +130,8 @@ public class MapFragment extends Fragment {
     }
 
     private void setZoom() {
-        //  Setteamos el zoom al mismo nivel y ajustamos la posición a un geopunto
         mapController = map.getController();
-        mapController.setZoom(15);
+        mapController.setZoom(14);
     }
 
     private void setOverlays() {
@@ -84,11 +150,13 @@ public class MapFragment extends Fragment {
             }
         });
 
+
 /*
         mMinimapOverlay = new MinimapOverlay(getContext(), map.getTileRequestCompleteHandler());
         mMinimapOverlay.setWidth(dm.widthPixels / 5);
         mMinimapOverlay.setHeight(dm.heightPixels / 5);
 */
+
 
         mScaleBarOverlay = new ScaleBarOverlay(map);
         mScaleBarOverlay.setCentred(true);
@@ -102,7 +170,7 @@ public class MapFragment extends Fragment {
         mCompassOverlay.enableCompass();
 
         map.getOverlays().add(myLocationOverlay);
-        map.getOverlays().add(this.mMinimapOverlay);
+        //map.getOverlays().add(this.mMinimapOverlay);
         map.getOverlays().add(this.mScaleBarOverlay);
         map.getOverlays().add(this.mCompassOverlay);
     }
