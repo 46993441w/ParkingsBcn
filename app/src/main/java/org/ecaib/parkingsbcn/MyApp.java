e674418dd4942bea74274a3b048d49c666dc42a5
lawer <carlesgm@gmail.com>
2016-02-10 08:57:00 +0100

    Activated Offline Cache

--- app/src/main/java/org/ecaib/parkingsbcn/MyApp.java
+++ app/src/main/java/org/ecaib/parkingsbcn/MyApp.java
@@ -2,7 +2,9 @@ package org.ecaib.parkingsbcn;
 
 import android.app.Application;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 /**
  * Created by carlesgm on 9/2/16.
@@ -15,16 +17,13 @@ public class MyApp extends Application {
         return ref;
     }
 
-    public void setRef(Firebase ref) {
-        this.ref = ref;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
 
         super.onCreate();
         Firebase.setAndroidContext(this);
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
 
         ref = new Firebase("https://parkingsbcn.firebaseio.com");
     }
